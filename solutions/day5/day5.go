0145ce6d5c62639f12a6128a12c7e78aea2c15c7
Roman Necheporenko <roman@waterfountain.one>
2023-12-05 15:33:13 +0200

    wip

--- solutions/day5/day5.go
+++ solutions/day5/day5.go
@@ -35,6 +35,94 @@ func (rm *RangeMap) Map(value int) (int, bool) {
 	return value, false
 }
 
+func (rm *RangeMap) MapRange(r *Range) ([]*Range, bool) {
+	ranges := []*Range{}
+	ok := true
+
+	src := rm.Src
+
+	if r.Start < src.Start {
+		if r.End() <= src.Start {
+			fmt.Printf("Range %s is to the left of %s\n", r.String(), src.String())
+			ranges = append(ranges, r)
+			ok = false
+		} else if r.End() <= src.End() {
+			fmt.Printf("Range %s (%d) left intersects %s, ", r.String(), r.Length, src.String())
+
+			outer := Range{
+				Start:  r.Start,
+				Length: src.Start - r.Start,
+			}
+
+			inner := Range{
+				Start:  rm.Dst.Start,
+				Length: r.Length - outer.Length,
+			}
+
+			ranges = append(ranges, &outer, &inner)
+
+			fmt.Printf(
+				"which remaps to %s (%d) and %s (%d)\n",
+				outer.String(), outer.Length,
+				inner.String(), inner.Length,
+			)
+
+		} else {
+			fmt.Printf("Range %s is larger than %s\n", r.String(), src.String())
+
+			leftOuter := Range{
+				Start:  r.Start,
+				Length: src.Start - r.Start,
+			}
+
+			inner := rm.Dst
+
+			rightOuter := Range{
+				Start:  src.End(),
+				Length: r.Length - src.End(),
+			}
+
+			ranges = append(ranges, &leftOuter, &inner, &rightOuter)
+		}
+	} else if r.Start < src.End() {
+		start, _ := rm.Map(r.Start)
+		if r.End() > src.End() {
+			fmt.Printf("Range %s (%d) right intersects %s, ", r.String(), r.Length, src.String())
+
+			inner := Range{
+				Start:  start,
+				Length: src.End() - r.Start,
+			}
+
+			outer := Range{
+				Start:  src.End(),
+				Length: r.Length - inner.Length,
+			}
+
+			ranges = append(ranges, &outer, &inner)
+
+			fmt.Printf(
+				"which remaps to %s (%d) and %s (%d)\n",
+				outer.String(), outer.Length,
+				inner.String(), inner.Length,
+			)
+
+		} else {
+			fmt.Printf("Range %s is inside %s\n", r.String(), src.String())
+			ranges = append(ranges, &Range{
+				Start:  start,
+				Length: r.Length,
+			})
+		}
+	} else {
+		fmt.Printf("Range %s is to the right of %s\n", r.String(), src.String())
+		ranges = append(ranges, r)
+		ok = false
+	}
+
+	return ranges, ok
+}
+
 type Section struct {
 	Src, Dst string
 	Maps     []RangeMap
@@ -49,6 +137,30 @@ func (s *Section) Map(value int) int {
 	}
 
 	return value
+}
+
+func (s *Section) MapRange(rng *Range) []*Range {
+	allRanges := []*Range{rng}
+
+	for _, rngMap := range s.Maps {
+		ranges, ok := rngMap.MapRange(rng)
+		if ok {
+			allRanges = append(allRanges, ranges...)
+		}
+
+		// if ok {
+		// 	return ranges
+		// }
+
+		// if ok {
+		// 	allRanges = append(allRanges, ranges...)
+		// } else if !check {
+		// 	allRanges = append(allRanges, ranges...)
+		// 	check = true
+		// }
+	}
+
+	return allRanges
 }
 
 func newSectionFromScanner(s *bufio.Scanner) *Section {
@@ -142,5 +254,64 @@ func PartA(s *bufio.Scanner) string {
 }
 
 func PartB(s *bufio.Scanner) string {
-	return ""
+	s.Scan()
+	seeds := extractNumbers(s.Text())
+
+	ranges := []*Range{}
+	for i, seed := range seeds {
+		if i % 2 != 0 {
+			continue
+		}
+
+		ranges = append(ranges, &Range{
+			Start:  seed,
+			Length: seeds[i+1],
+		})
+	}
+
+	s.Scan()
+
+	sections := make(map[string]*Section)
+	for {
+		sec := newSectionFromScanner(s)	
+		if sec == nil {
+			break
+		}
+		sections[sec.Src] = sec
+	}
+
+	category := "seed"
+	for {
+		section := sections[category]
+		if section == nil {
+			break
+		}
+
+		fmt.Printf("Section: %s\n", category)
+
+		newRanges := []*Range{}
+
+		for _, rng := range ranges {
+			result := section.MapRange(rng)
+			fmt.Printf("  Remap %s to %v\n", rng, result)
+			newRanges = append(newRanges, result...)
+		}
+
+		category = section.Dst
+		ranges = newRanges
+
+		fmt.Println()
+
+	}
+
+	minLocation := -1
+
+	for _, rng := range ranges {
+		// fmt.Printf("%d\n", rng.Start)
+		if minLocation == -1 || rng.Start < minLocation { //&& rng.Start > 0 {
+			minLocation = rng.Start
+		}
+	}
+
+	return strconv.Itoa(minLocation) 
 }
